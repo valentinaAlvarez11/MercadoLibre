c7317a50abf4218de68ea36dc31f711f72385154
Juan Triana <juanpablotriana03@gmail.com>
2025-09-17 08:06:37 -0500

    Finalización vista login

---
+++ mercadoLibreFront/app/login/page.tsx
@@ -0,0 +1,102 @@
+import React from 'react';
+import { FcGoogle } from 'react-icons/fc';
+import Image from 'next/image';
+import mercadolibreLogo from '../assets/mercadolibre1.png';
+
+const MercadolibreLogin = () => {
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center bg-white text-sm font-sans">
+      <header className="fixed top-0 left-0 w-full bg-yellow-300 shadow-md p-3 z-10">
+        <div className="container mx-auto px-9">
+          <Image 
+            src={mercadolibreLogo} 
+            alt="Mercado Libre Logo" 
+            width={130} 
+            height={50} 
+          />
+        </div>
+      </header>
+
+      <div className="flex justify-center items-center h-screen bg-white">
+        {/* Contenedor de la izquierda para el título */}
+        <div className="w-[500px] p-8 pt-15 mr-4 mb-60">
+          <h1 className="text-3xl font-semibold text-left">
+            Ingresa tu e-mail o teléfono para iniciar sesión
+          </h1>
+          <div className="mt-50">
+            <a href="#" className="inline-flex items-center space-x-2 rounded-lg shadow-md p-4 bg-white hover:bg-gray-100">
+              <div className="flex items-center space-x-2">
+                {/* Ícono de seguridad con signo de exclamación */}
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5 text-gray-500">
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                </svg>
+                <span>Tengo un problema de seguridad</span>
+              </div>
+              {/* Ícono de la flecha derecha */}
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4 text-gray-400">
+                <path strokeLinecap="round" strokeLinejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
+              </svg>
+            </a>
+            <a href="#" className="block mt-4 text-blue-500 text-sm hover:underline">
+              Necesito ayuda
+            </a>
+          </div>
+        </div>
+
+        {/* Contenedor de la derecha para el formulario */}
+        <div className="w-[470px] bg-white rounded-lg border border-gray-200 mb-55">
+          <div className="w-full p-8 pt-10">
+            <div className="mb-4">
+              <label htmlFor="email" className="block text-gray-700 text-sm mb-2">
+                E-mail o teléfono
+              </label>
+              <input
+                type="text"
+                id="email"
+                className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+              />
+            </div>
+
+            <button className="w-full bg-blue-500 text-white rounded-md py-3 text-lg font-semibold mb-3 hover:bg-blue-600 transition-colors cursor-pointer">
+              Continuar
+            </button>
+
+            <p className="text-center mb-4 w-full">
+              <a 
+                href="#" className="w-full py-4 text-blue-500 text-sm font-semibold rounded-md bg-transparent hover:bg-blue-50 transition-colors cursor-pointer block">
+                Crear cuenta
+              </a>
+            </p>
+
+            <div className="flex items-center my-4">
+              <div className="flex-grow h-px bg-gray-300"></div>
+              <span className="px-2 text-sm text-gray-500">o</span>
+              <div className="flex-grow h-px bg-gray-300"></div>
+            </div>
+
+            <button className="w-full bg-white border border-gray-300 rounded-md py-2 flex items-center justify-center space-x-2 hover:bg-gray-100 transition-colors">
+              <FcGoogle size={20} />
+              <span>Iniciar sesión con Google</span>
+            </button>
+
+            
+          </div>
+        </div>
+      </div>
+
+      <footer className="fixed bottom-0 left-0 w-full bg-gray-100 text-gray-500 text-xs p-7 flex justify-between items-center shadow-inner">
+        <div className="flex items-center space-x-4">
+          <a href="#" className="hover:underline text-blue-600">Cómo cuidamos tu privacidad</a>
+          <span>Copyright © 1999-2025 MercadoLibre Colombia LTDA.</span>
+        </div>
+        <div className="flex items-center space-x-4">
+          <span>Protegido por reCAPTCHA.</span>
+          <a href="#" className="hover:underline text-black">Privacidad</a>
+          <a href="#" className="hover:underline text-black">Condiciones</a>
+        </div>
+      </footer>
+    </div>
+  );
+};
+
+export default MercadolibreLogin;
